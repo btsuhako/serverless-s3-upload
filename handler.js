d0732982cfa61241eeb09c252c24ed8093fcb5eb
Blake Tsuhako <btsuhako@gmail.com>
2018-12-03 20:14:42 -0700

    updates

--- handler.js
+++ handler.js
@@ -5,20 +5,20 @@ const uploadsSign = require('./uploads-sign.js');
 console.log('starting event...');
 
 module.exports.sign = (event, context, callback) => {
-    uploadsSign(event, (error, result) => {
-        if (error) {
-            context.fail(error);
-            return;
-        }
-        const response = {
-            statusCode: 200,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'POST'
-            },
-            body: JSON.stringify(result)
-        };
-        context.succeed(response);
-    });
+  uploadsSign(event, (error, result) => {
+    if (error) {
+      context.fail(error);
+      return;
+    }
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+      },
+      body: JSON.stringify(result),
+    };
+    context.succeed(response);
+  });
 };
--- uploads-sign.js
+++ uploads-sign.js
@@ -3,7 +3,7 @@
 const aws = require('aws-sdk');
 const shortid = require('shortid');
 const s3 = new aws.S3({
-    apiVersion: '2006-03-01'
+  apiVersion: '2006-03-01',
 });
 require('dotenv').config();
 
@@ -13,41 +13,41 @@ const urlPrefix = process.env.SERVICE_S3_URL_PREFIX;
 console.log('uploads-sign is starting...');
 
 module.exports = (event, callback) => {
-    const body = JSON.parse(event.body);
-    if (!body.hasOwnProperty('contentType')) {
-        callback('no content type specified');
-        return;
-    } else if (!body.contentType.match(/(\.|\/)(gif|jpe?g|png)$/i)) {
-        callback('invalid content type, gif, jpg, and png supported');
-        return;
+  const body = JSON.parse(event.body);
+  if (!body.hasOwnProperty('contentType')) {
+    callback('no content type specified');
+    return;
+  } else if (!body.contentType.match(/(\.|\/)(gif|jpe?g|png)$/i)) {
+    callback('invalid content type, gif, jpg, and png supported');
+    return;
+  }
+
+  const extension = body.contentType.match(/(\.|\/)(gif|jpe?g|png)$/i)[2];
+  // Generate a random id
+  const fileBaseName = shortid.generate();
+  const key = fileBaseName + '.' + extension;
+
+  console.log('Event Type: ' + body.contentType);
+  console.log('Bucket name: ' + bucketName);
+  console.log('file key: ' + key);
+
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+    Body: '',
+    ContentType: body.contentType,
+    ACL: 'public-read',
+    Expires: 600,
+  };
+  s3.getSignedUrl('putObject', params, (err, url) => {
+    if (err) {
+      callback(err);
+      return;
     }
-
-    const extension = body.contentType.match(/(\.|\/)(gif|jpe?g|png)$/i)[2];
-    // Generate a random id
-    const fileBaseName = shortid.generate();
-    const key = fileBaseName + '.' + extension;
-
-    console.log('Event Type: ' + body.contentType);
-    console.log('Bucket name: ' + bucketName);
-    console.log('file key: ' + key);
-
-    const params = {
-        Bucket: bucketName,
-        Key: key,
-        Body: '',
-        ContentType: body.contentType,
-        ACL: 'public-read',
-        Expires: 600
+    const response = {
+      oneTimeUploadUrl: url,
+      resultUrl: urlPrefix + key,
     };
-    s3.getSignedUrl('putObject', params, (err, url) => {
-        if (err) {
-            callback(err);
-            return;
-        }
-        const response = {
-            oneTimeUploadUrl: url,
-            resultUrl: urlPrefix + key
-        };
-        callback(null, response);
-    });
+    callback(null, response);
+  });
 };
